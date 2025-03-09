a0e10dfa019b7df431e2d61eab2559d5b9c15d69
jakob.robnik@gmail.com <jakob.robnik@gmail.com>
2025-03-08 23:50:03 -0800

    trying to run emaus with shifte

--- sampler-comparison/sampler_comparison/experiments/emaus_experiment.py
+++ sampler-comparison/sampler_comparison/experiments/emaus_experiment.py
@@ -2,9 +2,18 @@ import os
 import jax
 jax.config.update("jax_enable_x64", True)
 
+from jax.lib import xla_bridge
+print(xla_bridge.get_backend().platform)
+#print(jax.extend.backend.get_backend)
+
 batch_size = 256
-os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=" + str(batch_size)
-num_cores = jax.local_device_count()
+#os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=" + str(batch_size)
+#num_cores = jax.local_device_count()
+
+import os, sys
+current_path = os.getcwd()
+sys.path.append(current_path + '/../../blackjax/')
+sys.path.append(current_path + '/../sampler-evaluation/')
 
 
 
@@ -16,9 +25,9 @@ from sampler_comparison.samplers.parallel.microcanonicalmontecarlo.emaus import 
 
 
 mesh = jax.sharding.Mesh(jax.devices(), 'chains')
+print('Number of devices: ', len(jax.devices()))
 
-
-parallel_microcanonical(num_steps1=100, num_steps2=400, num_chains=batch_size,mesh=mesh)(
+parallel_microcanonical(num_steps1=100, num_steps2=400, num_chains=batch_size, mesh=mesh)(
                 model=banana_mams_paper, num_steps=None, initial_position=None, key=jax.random.key(0)
                 )
 
