5be2d0f1cfcc7e63c811613cc063f2b27ec6b1c3
jakob.robnik@gmail.com <jakob.robnik@gmail.com>
2026-04-02 15:10:53 -0700

    code for stu

--- papers/uHMC/identifying_failure.py
+++ papers/uHMC/identifying_failure.py
@@ -1,57 +1,93 @@
 import os, sys
+os.environ["JAX_PLATFORMS"] = "cpu"
 import jax
 import jax.numpy as jnp
 jax.config.update("jax_enable_x64", True)
-#os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=" + str(batch_size)
+os.environ["XLA_FLAGS"] = "--xla_force_host_platform_device_count=" + str(32)
 sys.path.append('../blackjax/')
 sys.path.append('sampler-evaluation/')
 sys.path.append('sampler-comparison/')
 import blackjax
-from blackjax.util import thin_algorithm, store_only_expectation_values, run_inference_algorithm
-from sampler_evaluation.models.student_t import StudentT
+from blackjax.util import store_only_expectation_values, run_inference_algorithm
+from sampler_evaluation.models.student_t import StudentT, Power
+import numpy as np
+from blackjax.base import SamplingAlgorithm
+
 
 num_cores = jax.local_device_count()
-print(num_cores)
 
 
-def frobenious(model):
+
+def thin_algorithm(sampling_algorithm, thinning = 1):
+
+    def step_fn(rng_key, state):
+        step = lambda state, rng_key: sampling_algorithm.step(rng_key, state)
+        keys = jax.random.split(rng_key, thinning)
+        return jax.lax.scan(step, state, keys)
+
+    return SamplingAlgorithm(sampling_algorithm.init, step_fn)
+
+
+def frobenious(inv_cov):
     """directly convert the covariance matrix expectation values to the bias scalar (to save memory)"""
     
     def func(cov):
-        residual = jnp.eye(model.ndims) - model.inv_cov @ cov
+        residual = jnp.eye(len(inv_cov)) - inv_cov @ cov
         return jnp.average(jnp.diag(residual @ residual))
     
     return func
 
 
 
-def run_mclmc(model, step_size, L, steps, key):
+def run_mclmc(model, inv_cov, steps, key, 
+              desired_energy_variance = 1e-3):
 
     num_saved_steps = 1000
 
-    init_key, run_key = jax.random.split(key, 2)
+    init_key, tune_key, run_key = jax.random.split(key, 3)
 
     initial_position = model.sample_init(init_key)
+    initial_state = blackjax.mcmc.mclmc.init(initial_position, model.log_density_fn, init_key)
 
-    # create an initial state for the sampler
-    initial_state = blackjax.mcmc.mclmc.init(
-        position=initial_position, logdensity_fn=model.logdensity_fn, rng_key=init_key
+    integrator = blackjax.mcmc.integrators.isokinetic_velocity_verlet
+    
+    kernel = lambda inverse_mass_matrix : blackjax.mcmc.mclmc.build_kernel(
+        logdensity_fn=model.log_density_fn,
+        integrator= integrator,
+        inverse_mass_matrix=inverse_mass_matrix,
     )
 
-    # use the quick wrapper to build a new kernel with the tuned parameters
+    # find values for L and step_size
+    (
+        blackjax_state_after_tuning,
+        blackjax_mclmc_sampler_params,
+        _
+    ) = blackjax.mclmc_find_L_and_step_size(
+        mclmc_kernel=kernel,
+        num_steps= 10000,
+        state=initial_state,
+        diagonal_preconditioning=False,
+        rng_key = tune_key,
+        desired_energy_var=desired_energy_variance,
+    )
+
     sampling_alg = blackjax.mclmc(
-        model.logdensity_fn,
-        L= L,
-        step_size= step_size,
+        model.log_density_fn,
+        L= blackjax_mclmc_sampler_params.L,
+        step_size= blackjax_mclmc_sampler_params.step_size,
+        integrator = integrator
     )
 
+        
     sampling_alg, transform = store_only_expectation_values(
                 sampling_algorithm= sampling_alg,
-                state_transform=frobenious(model), burnin = steps // 4)
+                incremental_value_transform = frobenious(inv_cov),
+                state_transform= lambda state: jnp.outer(state.position, state.position), 
+                burn_in = 0)
     
     sampling_alg = thin_algorithm(sampling_alg, thinning = steps // num_saved_steps)
 
-    initial_state = sampling_alg.init(initial_state)
+    initial_state = sampling_alg.init(blackjax_state_after_tuning)
 
     bias, info = run_inference_algorithm(
              rng_key=run_key,
@@ -59,7 +95,7 @@ def run_mclmc(model, step_size, L, steps, key):
              inference_algorithm=sampling_alg,
              num_steps= num_saved_steps,
              transform=transform,
-             progress_bar=True,
+             progress_bar= True,
          )[1]
 
     eevpd = jnp.std(info.energy_change)**2 / model.ndims
@@ -70,9 +106,25 @@ def run_mclmc(model, step_size, L, steps, key):
 
 if __name__ == '__main__':
 
-    model = StudentT(ndims= 100, dof= 5)
-    bias, eevpd = run_mclmc(model, step_size= 2., L= 10, steps=10**4)
+    imodel = int(sys.argv[1])
+    Imodel = int(sys.argv[2])
 
-    jnp.savez('junk.npy', bias= bias, eevpd= eevpd)
+    if Imodel == 0:
+        dof = [ 3,  4,  5,  6,  7,  8,  9, 10, 12, 14, 18, 22, 27, 33, 40, 50][imodel]
+        model, inv_cov = StudentT(ndims= 100, dof= dof)
+    else:
+        p = np.arange(2, 10)[imodel]
+        model, inv_cov = Power(ndims= 100, p = p)
+
+    key = jax.random.key(0)
+    keys= jax.random.split(key, 32)
+
+    
+    bias, eevpd = jax.pmap(lambda key: run_mclmc(model, inv_cov, steps=10**7, key = key))(keys)
+    
+    scratch = '/pscratch/sd/j/jrobnik/bias/'
+
+    jnp.savez(scratch + model.name, bias= bias, eevpd= eevpd)
+
 
 #shifter --image=jrobnik/sampling:1.0 python3 -m papers.uHMC.identifying_failure
--- sampler-evaluation/sampler_evaluation/models/student_t.py
+++ sampler-evaluation/sampler_evaluation/models/student_t.py
@@ -10,19 +10,18 @@ def StudentT(ndims, dof):
 
     e_x2 = jnp.ones(ndims) * dof / (dof - 2.)
     
-    sample_init = lambda key: jax.random.t(key, dof = dof, shape=(ndims,))
+    sample_init = lambda key: jax.random.t(key, df= dof, shape= (ndims,))
 
     sample_transformations = {  
         "identity": SampleTransformation(
             fn=lambda params: params,
-            pretty_name="Identity",
             ground_truth_mean=jnp.zeros(ndims),
             ground_truth_standard_deviation=jnp.sqrt(e_x2)), 
-        "covariance": SampleTransformation(
-            fn=lambda params: jnp.outer(params, params),
-            pretty_name="Covariance",
-            ground_truth_mean= jnp.diagonal(e_x2),
-            ground_truth_standard_deviation=jnp.nan)
+        # "covariance": SampleTransformation(
+        #     fn=lambda params: jnp.outer(params, params),
+        #     name="Covariance",
+        #     ground_truth_mean= jnp.diagonal(e_x2),
+        #     ground_truth_standard_deviation=jnp.nan)
     }
     
     model = make_model(
@@ -31,13 +30,13 @@ def StudentT(ndims, dof):
             default_event_space_bijector= lambda x: x,
             sample_transformations = sample_transformations,
             exact_sample= sample_init,
-            name=f"Student-t_{dof=}",
-            pretty_name= f"Student - t (dof = {dof})",    
+            name=f"Student-t_dof={dof}",
             sample_init = sample_init
             )
-    model.inv_cov = jnp.diagonal(1./e_x2)
+    
+    inv_cov = jnp.diag(1./e_x2)
 
-    return model
+    return model, inv_cov
 
 
 
@@ -50,14 +49,8 @@ def Power(ndims, p):
     sample_transformations = {  
         "identity": SampleTransformation(
             fn=lambda params: params,
-            pretty_name="Identity",
             ground_truth_mean=jnp.zeros(ndims),
-            ground_truth_standard_deviation=jnp.sqrt(e_x2)), 
-        "covariance": SampleTransformation(
-            fn=lambda params: jnp.outer(params, params),
-            pretty_name="Covariance",
-            ground_truth_mean= jnp.diagonal(e_x2),
-            ground_truth_standard_deviation=jnp.nan)
+            ground_truth_standard_deviation=jnp.sqrt(e_x2))
     }
     
     model = make_model(
@@ -65,12 +58,10 @@ def Power(ndims, p):
             ndims=ndims,
             default_event_space_bijector= lambda x: x,
             sample_transformations = sample_transformations,
-            exact_sample= sample_init,
             name=f"Power_{p}",
-            pretty_name= f"Power ({p=})",
             sample_init = sample_init
             )
     
-    model.inv_cov = jnp.diagonal(1./e_x2)
+    inv_cov = jnp.diag(1./e_x2)
 
-    return model
+    return model, inv_cov
